bc7724628468733f5ab9b9936b449998cf2d74d6
nishant920 <nishantdhiman044@gmail.com>
2026-04-19 22:45:04 +0530

    Move to dom manipulation - to do list project

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
 
+let tasks = [];
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const value = input.value;
+
+    if (value === "") return;
+
+    tasks.push({
+        title: value,
+        completed: false
+    });
+
+    input.value = "";
+    renderTask();
+}
+
+function renderTask() {
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerText = task.title;
+
+        const btn = document.createElement("button");
+        btn.innerText = "Delete";
+
+        btn.onclick = function () {
+            tasks.splice(index, 1); 
+            renderTask();
+        };
+
+        li.appendChild(btn);
+        list.appendChild(li);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
 //ADD
 //let a = 9;
 //let b = 19;
@@ -50,15 +99,47 @@
 //this keyword for arrow function and normal funtion behaves diffrently 
 // while for normal funtion it mainly points to object that owns the methode but for 
 // but arrow don't have their own this they inherit it from surrending and lexical scope(window object)
+/*
+arr = ["a", "b", "c"]
 
-//arr = ["a", "b", "c"]
-//
-//function removeAtIndex(arr, index){
-//  arr.splice(index, 1);
-//  return arr;
-//}
-//
-//console.log(removeAtIndex(arr, 1));
-//
+function removeAtIndex(arr, index){
+  arr.splice(index, 2);
+  return arr;
+}
+
+console.log(removeAtIndex(arr, 0));
+
 //It is important to know that splice() is a mutating method. This means it physically changes the original array in your computer's memory
 
+/*
+let task ={
+    title: "Study",
+    completed: false
+};
+//toggle
+task.completed = !task.completed;
+console.log(task.completed);
+
+
+let task =[
+    {title: "Study", completed: false}, {title: "gym", completed: true}
+];
+
+function toggleTask(index) {
+    task[index].completed = !task[index].completed;
+    return task[index];
+}
+console.log(task);
+console.log(toggleTask(0));
+
+
+let tasks = [];
+
+function addTasks(task){
+tasks.push(task);
+}
+
+addTasks("Study");
+
+console.log(tasks);
+*/
