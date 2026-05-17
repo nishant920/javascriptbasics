a96144472c6f265d13f0f20faf61b70fbafddbf4
nishant920 <nishantdhiman044@gmail.com>
2026-05-17 23:14:38 +0530

    Search movies script

---
+++ Project5/script.js
@@ -0,0 +1,30 @@
+const searchBox = document.querySelector(".search-box");
+
+searchBox.addEventListener("input", function(){
+    const searchText = searchBox.value.toLowerCase();
+    const moviesSection = document.querySelectorAll(".movies-section");
+
+    moviesSection.forEach(function(section){
+        const movieCards = section.querySelectorAll(".movie-card");
+        const noResult = section.querySelector(".no-results");
+        let visibleCount = 0;
+    
+
+    movieCards.forEach(function(card){
+        const movieTitle = card.querySelector("h3").textContent.toLowerCase();
+    
+    if(movieTitle.includes(searchText)){
+        card.classList.remove("hidden");
+        visibleCount++;
+        console.log("Total number of movies for this search", visibleCount);
+    }else {
+        card.classList.add("hidden");
+    }
+  });
+  if(visibleCount === 0){
+    noResult.style.display = "block";
+  } else{
+    noResult.style.display = "none";
+  }
+});
+});
